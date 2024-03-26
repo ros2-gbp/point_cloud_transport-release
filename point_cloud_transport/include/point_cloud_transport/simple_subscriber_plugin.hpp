b76c869c88e0df67ca30404d455380d50391d966
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-03-26 10:32:46 +0100

    Imported upstream version '2.0.5' of 'upstream'

--- point_cloud_transport/include/point_cloud_transport/simple_subscriber_plugin.hpp
+++ point_cloud_transport/include/point_cloud_transport/simple_subscriber_plugin.hpp
@@ -117,7 +117,7 @@ public:
   }
 
   void setParamCallback(
-    rclcpp::node_interfaces::NodeParametersInterface::OnParametersSetCallbackType
+    rclcpp::node_interfaces::NodeParametersInterface::OnSetParametersCallbackType
     param_change_callback)
   {
     if (impl_) {
